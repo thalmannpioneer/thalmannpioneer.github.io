ad4f787dc3684a49b7140c1dec38efa6af4cfe02
thalmannpioneer <82112296+commiebasil@users.noreply.github.com>
2025-11-01 18:56:13 +0300

    Main commit

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//переменные
+let startingOverlayContainer = document.getElementsByClassName("starting-overlay")[0];
+let qrContainer = document.getElementsByClassName("telegram-qr-overlay")[0];
+let clipboardNotificationContainer = document.getElementsByClassName("clipboard-notification")[0];
+
+//функции
+function telegramQrShow() {
+        qrContainer.classList.add("telegram-qr-overlay_shown");
+}
+function telegramQrHide() {
+        qrContainer.classList.remove("telegram-qr-overlay_shown");
+}
+
+function discordIdCopy() {
+        navigator.clipboard.writeText("thalmannpioneer");
+
+        clipboardNotificationContainer.classList.add("clipboard-notification_shown");
+        setTimeout(() => { clipboardNotificationContainer.classList.remove("clipboard-notification_shown"); }, 1000);
+}
+
+//действия при загрузке
+startingOverlayContainer.classList.add("starting-overlay_hidden");
